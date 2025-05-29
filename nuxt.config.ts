3796f477dc3ba2d7a457600c05bca83ade48a173
Leo Prada <leo.prada90@gmail.com>
2025-05-29 04:28:28 -0500

    test base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  app: {
+    baseURL: '/atm-application/'
+  },
   typescript: {
     typeCheck: true,
     strict: true,
